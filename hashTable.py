b7c7372724812b94c9bc44cc2d580b7293282bc8
lorenzo-lavacchini <lorenzo.lavacchini@gmail.com>
2023-10-14 17:34:59 +0200

    Added hashTable.py class

---
+++ hashTable.py
@@ -0,0 +1,30 @@
+import LinkedList
+from LinkedList import element
+from LinkedList import listConc
+class hashTable:
+   
+    #m -> grandezza della tabella
+    #n -> numero di chiavi da memorizzare nella tabella (cardinalità di K)
+    def __init__(self,n):
+        self.m = (int)(n / 0.8)
+        self.T = [listConc]*self.m
+        for i in range(self.m):
+            self.T[i] = listConc()
+
+
+    def hashFunction(self, k):
+        return k % self.m
+
+    def insert(self,k):
+        self.T[self.hashFunction(k)].insert(element(k))
+
+    def search(self,k):
+        self.T[self.hashFunction(k)].search(k)
+
+    def delete(self,k):
+        self.T[self.hashFunction(k)].delete(k)
+
+    def printHashTable(self):
+        for i in range(0,len(self.T)):
+            print("Lista n°",i)
+            print(self.T[i].printAll())
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from ListaConcatenata import element
-from ListaConcatenata import listConc
+from LinkedList import element
+from LinkedList import listConc
 from ABR import node
 from ABR import ABR
 from timeit import default_timer as timer
 import random
 import matplotlib.pyplot as plt
+from hashTable import hashTable
 if __name__ == '__main__':
-
-
+    n = 50
+    table = hashTable(n)
+    table.insert(6)
+    table.insert(68)
+    table.printHashTable()
 
